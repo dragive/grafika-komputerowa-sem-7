84678173dc591040f8ec473e282269b4917538bd
dragive <maciejf@int.pl>
2022-10-31 13:11:27 +0100

    Dodanie do 4 filtra niepunktowego

--- src/fourth/Main.py
+++ src/fourth/Main.py
@@ -6,6 +6,7 @@ from tkinter import filedialog, messagebox, HORIZONTAL
 from typing import Any, Callable, Tuple
 
 import PIL.ImageTk
+import numpy as np
 from PIL import ImageTk
 from PIL.Image import Image, NEAREST
 
@@ -163,6 +164,256 @@ class MainWindow(tk.Tk):
         MainWindow().initialize_view().mainloop()
 
 
+class FilterMedian:
+    def filter(self, im: PIL.Image.Image):
+        im.rankfilter()
+
+
+class ThreeXThreeTransformation:
+    @abstractmethod
+    def transform_pixel(self, pixel: Tuple[
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+    ]) -> tuple[int, int, int]:
+        pass
+
+    def apply(self, image: PIL.Image.Image):
+        data = image.getdata().pixel_access()
+
+        width, height = image.width, image.height
+
+        def get_pixels(w, h):
+            if w == 0:
+                if h == 0:
+                    return (
+                        (
+                            data[w, h],
+                            data[w + 1, h],
+                        ),
+                        (
+                            data[w, (h + 1)],
+                            data[w + 1, (h + 1)],
+                        ),
+                    )
+                elif h >= height - 1:
+                    return (
+                        (
+                            data[w, (h - 1)],
+                            data[w + 1, (h - 1)],
+                        ),
+                        (
+                            data[(w, h)],
+                            data[(w + 1, h)],
+                        ),
+                    )
+                else:
+                    return (
+                        (
+                            data[w, (h - 1)],
+                            data[w + 1, (h - 1)],
+                        ),
+                        (
+                            data[w, h],
+                            data[w + 1, h],
+                        ),
+                        (
+                            data[w, (h + 1)],
+                            data[w + 1, (h + 1)],
+                        ),
+                    )
+            elif w >= width - 1:
+                if h == 0:
+                    return (
+                        (
+                            data[w - 1, h],
+                            data[w, h],
+                        ),
+                        (
+                            data[w - 1, h + 1,],
+                            data[w, h + 1],
+                        ),
+                    )
+                elif h >= height - 1:
+                    return (
+                        (
+                            data[w - 1, (h - 1)],
+                            data[w, (h - 1)],
+                        ),
+                        (
+                            data[w - 1, h],
+                            data[w, h],
+                        ),
+                    )
+                else:
+                    return (
+                        (
+                            data[w - 1, h - 1],
+                            data[w, h - 1],
+                        ),
+                        (
+                            data[w - 1, h],
+                            data[w, h],
+                        ),
+                        (
+                            data[w - 1, h + 1],
+                            data[w, h + 1],
+                        ),
+                    )
+
+            if h == 0:
+                return (
+                    (
+                        data[w - 1, h],
+                        data[w, h],
+                        data[w + 1, h],
+                    ),
+                    (
+                        data[w - 1, h + 1,],
+                        data[w, h + 1],
+                        data[w + 1, h + 1],
+                    ),
+
+                )
+            elif h >= height - 1:
+                return (
+                    (
+                        data[w - 1, h - 1],
+                        data[w, h - 1],
+                        data[w + 1, h - 1],
+                    ),
+                    (
+                        data[w - 1, h],
+                        data[w, h],
+                        data[w + 1, h],
+                    )
+
+                )
+
+            return (
+                (
+                    data[w - 1, h - 1],
+                    data[w, h - 1],
+                    data[w + 1, h - 1],
+                ),
+                (
+                    data[w - 1, h],
+                    data[w, h],
+                    data[w + 1, h],
+                ),
+                (
+                    data[w - 1, h + 1,],
+                    data[w, h + 1],
+                    data[w + 1, h + 1],
+                ),
+
+            )
+
+        data = [
+            self.transform_pixel(get_pixels(w, h))
+            for h in range(height)
+            for w in range(width)
+
+        ]
+        image.putdata(data)
+
+
+class MedianFilter(ThreeXThreeTransformation):
+    @abstractmethod
+    def transform_pixel(self, pixel: Tuple[
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],]) -> \
+            tuple[int, int, int]:
+        pixel = (
+            int(median([x[0] for row in pixel for x in row])),
+            int(median([x[1] for row in pixel for x in row])),
+            int(median([x[2] for row in pixel for x in row])),
+        )
+
+        return pixel
+
+
+class MeanFilter(ThreeXThreeTransformation):
+    @abstractmethod
+    def transform_pixel(self, pixel: Tuple[
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],]) -> \
+            tuple[int, int, int]:
+        a, b, c = [x[0] for row in pixel for x in row], \
+                  [x[1] for row in pixel for x in row], \
+                  [x[2] for row in pixel for x in row],
+
+        pixel = (
+            sum(a) // len(a),
+            sum(b) // len(b),
+            sum(c) // len(c),
+        )
+
+        return pixel
+
+
+class SobelFilter(ThreeXThreeTransformation):
+    @abstractmethod
+    def transform_pixel(self, pixel: Tuple[
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],]) -> \
+            tuple[int, int, int]:
+
+        if len(pixel) != 3:
+            return (0, 0, 0)
+
+        if len(pixel[0]) != 3:
+            return (0, 0, 0)
+
+        x = np.array(
+            [
+                [1, 0, -1],
+                [2, 0, -2],
+                [1, 0, -1],
+            ]
+        )
+        y = x.T
+
+        pixel = np.array(pixel)
+
+        gx = x.dot(pixel)
+        gy = pixel.dot(y)
+
+        return tuple(int(x) for x in np.sqrt(np.power(gx, 2) + np.power(gy, 2))[2, 2])
+
+
+class LowPassFilter(ThreeXThreeTransformation):
+    @abstractmethod
+    def transform_pixel(self, pixel: Tuple[
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],
+        tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]],]) -> \
+            tuple[int, int, int]:
+
+        if len(pixel) != 3:
+            return (0, 0, 0)
+
+        if len(pixel[0]) != 3:
+            return (0, 0, 0)
+
+        x = np.array(
+            [
+                [0, -1, 0],
+                [-1, 5, -1],
+                [0, -1, 0],
+            ]
+        )
+
+        pixel = np.array(pixel)
+
+        gx = x.dot(pixel)
+
+        return tuple(int(x) for x in gx)[2, 2]
+
+
 class PointTransformation:
     @abstractmethod
     def transform_pixel(self, pixel: tuple[int, int, int], value: tuple[float, float, float]) -> tuple[int, int, int]:
@@ -254,14 +505,17 @@ class FilterSettingsTopLevel(tk.Toplevel):
 
     def _init_button_array(self):
         self.entry_value: tk.Entry = tk.Entry(self, width=100)
-        self.entry_value.grid(row=0, column=0, sticky='nesw')
+        row = 0
+        self.entry_value.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
         self.button_add_value: tk.Button = tk.Button(self,
                                                      text="Add value",
                                                      width=WIDTH_OF_BUTTONS,
                                                      command=
                                                      self.get_command_pixel_transformation(AddPointTransformation))
-        self.button_add_value.grid(row=1, column=0, sticky='nesw')
+        self.button_add_value.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
         self.button_subtract_value: tk.Button = tk.Button(self,
                                                           text="Subract value",
@@ -269,7 +523,8 @@ class FilterSettingsTopLevel(tk.Toplevel):
                                                           command=
                                                           self.get_command_pixel_transformation(
                                                               SubtractPointTransformation))
-        self.button_subtract_value.grid(row=2, column=0, sticky='nesw')
+        self.button_subtract_value.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
         self.button_multiply_value: tk.Button = tk.Button(self,
                                                           text="Multiply value",
@@ -277,7 +532,8 @@ class FilterSettingsTopLevel(tk.Toplevel):
                                                           command=
                                                           self.get_command_pixel_transformation(
                                                               MultiplyPointTransformation))
-        self.button_multiply_value.grid(row=3, column=0, sticky='nesw')
+        self.button_multiply_value.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
         self.button_divide_value: tk.Button = tk.Button(self,
                                                         text="Divide value",
@@ -285,7 +541,8 @@ class FilterSettingsTopLevel(tk.Toplevel):
                                                         command=
                                                         self.get_command_pixel_transformation(
                                                             DividePointTransformation))
-        self.button_divide_value.grid(row=4, column=0, sticky='nesw')
+        self.button_divide_value.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
         self.button_gray_scale_value: tk.Button = tk.Button(self,
                                                             text="Gray value",
@@ -293,7 +550,8 @@ class FilterSettingsTopLevel(tk.Toplevel):
                                                             command=
                                                             self.get_command_pixel_transformation(
                                                                 GrayScalePointTransformation))
-        self.button_gray_scale_value.grid(row=5, column=0, sticky='nesw')
+        self.button_gray_scale_value.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
         self.button_gray_scale_value2: tk.Button = tk.Button(self,
                                                              text="Gray2 value",
@@ -301,12 +559,51 @@ class FilterSettingsTopLevel(tk.Toplevel):
                                                              command=
                                                              self.get_command_pixel_transformation(
                                                                  Gray2ScalePointTransformation))
-        self.button_gray_scale_value2.grid(row=6, column=0, sticky='nesw')
+        self.button_gray_scale_value2.grid(row=row, column=0, sticky='nesw')
+        row += 1
+
+        self.button_space_1: tk.Label = tk.Label(self, text=" ")
+        self.button_space_1.grid(row=row, column=0, sticky='nesw')
+        row += 1
+
+        self.button_median_filter: tk.Button = tk.Button(self,
+                                                         text="Median value",
+                                                         width=WIDTH_OF_BUTTONS,
+                                                         command=
+                                                         self.get_command_filter_transformation(
+                                                             MedianFilter))
+        self.button_median_filter.grid(row=row, column=0, sticky='nesw')
+        row += 1
+
+        self.button_mean_filter: tk.Button = tk.Button(self,
+                                                       text="Mean value",
+                                                       width=WIDTH_OF_BUTTONS,
+                                                       command=
+                                                       self.get_command_filter_transformation(
+                                                           MeanFilter))
+        self.button_mean_filter.grid(row=row, column=0, sticky='nesw')
+        row += 1
+
+        self.button_sobel_filter: tk.Button = tk.Button(self,
+                                                        text="Sobel value",
+                                                        width=WIDTH_OF_BUTTONS,
+                                                        command=
+                                                        self.get_command_filter_transformation(
+                                                            SobelFilter))
+        self.button_sobel_filter.grid(row=row, column=0, sticky='nesw')
+        row += 1
 
     def get_command_pixel_transformation(self, name_of_tool: type(PointTransformation)) -> Callable[[Any], Any]:
         return lambda *args, **kwargs: (
             name_of_tool()
             .apply(self.master.image_from_pixels, self.entry_value.get()),
+            self.master.redraw(self.master.image_from_pixels)
+        )
+
+    def get_command_filter_transformation(self, name_of_tool: type(ThreeXThreeTransformation)) -> Callable[[Any], Any]:
+        return lambda *args, **kwargs: (
+            name_of_tool()
+            .apply(self.master.image_from_pixels),
             self.master.redraw(self.master.image_from_pixels)
         )
 
